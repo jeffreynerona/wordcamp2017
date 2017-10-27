4571147cedbf26ea99ca5b6a180f6fd3ef3d5776
jeffreynerona <jeffnerona@gmail.com>
2017-10-27 17:08:47 +0800

    Added a readme

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
+import { DropDownNav, Presentation, PresenterModePlugin, Slide } from 'react-presents';
 import './App.css';
+
+const slides = require.context('./slides/', false, /\.js$/)
+  .keys()
+  .map((filename) => filename.replace('./', ''))
+  .map((filename) => require(`./slides/${filename}`).default)
+
+const options = slides
+  .map((slide, index) => ({
+	label: slide.title,
+	value: index
+  }))
+.filter((option) => option.label)
 
 class App extends Component {
   render() {
     return (
-      <div>
-       
-      </div>
+     <Presentation>
+	    <PresenterModePlugin />
+
+	    {slides.map((Component, index) => (
+	      <Slide
+	        component={Component}
+	        key={index}
+	      />
+	    ))}
+
+	    <DropDownNav
+	      key='DropDownNav'
+	      options={options}
+	    />
+	  </Presentation>
     );
   }
 }
